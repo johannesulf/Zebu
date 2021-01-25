dc80bddea7318945424b8547f0c5b8f07d449b62
johannesulf <jolange@ucsc.edu>
2021-01-25 12:19:55 -0800

    updated lwb

--- lwb/compute.py
+++ lwb/compute.py
@@ -7,24 +7,14 @@ from dsigma.precompute import precompute_catalog, add_maximum_lens_redshift
 from dsigma.jackknife import add_continous_fields, jackknife_field_centers
 from dsigma.jackknife import add_jackknife_fields, jackknife_resampling
 from dsigma.stacking import excess_surface_density
-from astropy.cosmology import FlatLambdaCDM
 from dsigma.surveys import kids
+from astropy.cosmology import FlatLambdaCDM
 
-parser = argparse.ArgumentParser(description='Calculate the galaxy-galaxy ' +
-                                 'lensing signal for the LWB project.')
+parser = argparse.ArgumentParser(
+    description='Calculate the lensing signal for the LWB project.')
 
 parser.add_argument('survey', help='the lens survey')
-parser.add_argument('--no_dilution_correction', action='store_true',
-                    help='do not correct for photo-z dilution')
-parser.add_argument('--nstar', help='whether to calculate only the high or ' +
-                    'low n_star regions')
 args = parser.parse_args()
-
-if args.nstar is not None:
-    if args.nstar not in ['low', 'high']:
-        raise ValueError("nstar must be 'low' or 'high'.")
-    if args.survey.lower() != 'cfht':
-        raise ValueError("nstar can only be specified for CFHT.")
 
 # %%
 
@@ -32,25 +22,21 @@ if args.survey.lower() == 'kids':
 
     table_s = Table()
 
-    for reg in [9, 12, 15, 23, 'S']:
-        kv = Table.read(os.path.join(
-            'kids', 'KV450_G{}_reweight_3x4x4_v2_good.cat'.format(reg)), hdu=1)
-        kv = kv[kv['MASK'] == 0]
+    for region in [9, 12, 15, 23, 'S']:
+        table_s = vstack([table_s, Table.read(os.path.join(
+            'raw', 'KV450_G{}_reweight_3x4x4_v2_good.cat'.format(region)),
+                                              hdu=1)],
+                         metadata_conflicts='silent')
 
-        table = dsigma_table(kv, 'source', survey='KiDS', version='KV450',
-                             verbose=reg == 9)
-
-        for key in table.colnames:
-            table[key].unit = None
-        table = Table(table)
-
-        table_s = vstack([table_s, table])
+    table_s = table_s[table_s['MASK'] == 0]
+    table_s = dsigma_table(table_s, 'source', survey='KiDS', version='KV450')
 
     z_bins = [0.1, 0.3, 0.5, 0.7, 0.9, 1.2]
     table_s['z_bin'] = np.digitize(table_s['z'], z_bins) - 1
 
     for i in range(len(z_bins) - 1):
-        data = Table.read('Nz_DIR_z{}t{}.asc'.format(z_bins[i], z_bins[i + 1]),
+        data = Table.read(os.path.join(
+            'raw', 'Nz_DIR_z{}t{}.asc'.format(z_bins[i], z_bins[i + 1])),
                           format='ascii', names=['z', 'n(z)'])
         if i == 0:
             nz_z = data['z']
@@ -61,16 +47,19 @@ if args.survey.lower() == 'kids':
 
     table_s = table_s[(table_s['z_bin'] >= 0) &
                       (table_s['z_bin'] < len(z_bins) - 1)]
+    table_s = add_maximum_lens_redshift(table_s, dz_min=0.2)
+    table_s['m'] = kids.multiplicative_shear_bias(table_s['z'],
+                                                  version='KV450')
 
 
 elif args.survey.lower() == 'cfht':
 
-    table_s = Table.read('CFHTLS.csv')
+    table_s = Table.read(os.path.join('raw', 'CFHTLS.csv'))
     table_c = table_s[np.random.randint(len(table_s), size=100000)]
     table_s = dsigma_table(table_s, 'source', survey='cfhtls')
     table_s = table_s[table_s['z'] > 0.25 - 1e-6]
     table_s = table_s[table_s['z'] < 1.3 + 1e-6]
-    table_s = add_maximum_lens_redshift(table_s, dz_min=0.105)
+    table_s = add_maximum_lens_redshift(table_s, dz_min=0.2)
 
     z_pdf = np.linspace(0.0, 3.5, 71)
     z_pdf = 0.5 * (z_pdf[1:] + z_pdf[:-1]) + 0.005
@@ -92,14 +81,6 @@ elif args.survey.lower() == 'cfht':
 else:
     raise ValueError("Survey must be 'kids' or 'cfht'.")
 
-elif args.survey.lower() == 'cfht':
-    if args.no_dilution_correction:
-        print("Not correcting for photo-z dilution.")
-        table_c['z_l_max'] = np.minimum(table_c['z_l_max'],
-                                        table_c['z_true'] - 1e-5)
-    else:
-        print("Correcting for photo-z dilution.")
-
 # %%
 
 rp_bins = np.logspace(np.log10(0.05), np.log10(15.), 11)
@@ -107,40 +88,27 @@ z_bins = [0.15, 0.31, 0.43, 0.54, 0.70]
 
 for lens_bin in range(4):
 
-    table_l = Table.read(
-        'lwb_files_jan2020/{}_{}_{}_{}.dat'.format(
-            args.survey.upper(), 'LOWZ' if lens_bin < 2 else 'CMASS',
-            z_bins[lens_bin], z_bins[lens_bin + 1]), delimiter=' ',
-        format='ascii', names=['ra', 'dec', 'z', 'w_sys', 'w_tot', 'm_star'])
+    table_l = Table.read(os.path.join('raw', '{}_{}_{}_{}.dat'.format(
+        args.survey.upper(), 'LOWZ' if lens_bin < 2 else 'CMASS',
+        z_bins[lens_bin], z_bins[lens_bin + 1])), delimiter=' ',
+                         format='ascii', names=['ra', 'dec', 'z', 'w_sys',
+                                                'w_tot', 'm_star'])
 
-    table_r = Table.read(
-        'lwb_files_jan2020/{}_{}_RANDOMS_{}_{}.dat'.format(
+    table_r = Table.read(os.path.join('raw', '{}_{}_RANDOMS_{}_{}.dat'.format(
             args.survey.upper(), 'LOWZ' if lens_bin < 2 else 'CMASS',
-            z_bins[lens_bin], z_bins[lens_bin + 1]), delimiter=' ',
-        format='ascii', names=['ra', 'dec', 'z'])
-
-    if args.nstar is not None:
-        mask_l = ((table_l['ra'] < 100) | ((table_l['ra'] > 200) &
-                                           (table_l['ra'] < 300)))
-        mask_r = ((table_r['ra'] < 100) | ((table_r['ra'] > 200) &
-                                           (table_r['ra'] < 300)))
-        if args.nstar == 'low':
-            table_l = table_l[mask_l]
-            table_r = table_r[mask_r]
-        else:
-            table_l = table_l[~mask_l]
-            table_r = table_r[~mask_r]
+            z_bins[lens_bin], z_bins[lens_bin + 1])), delimiter=' ',
+                         format='ascii', names=['ra', 'dec', 'z'])
 
     table_r['w_sys'] = np.ones(len(table_r))
     table_l['w_sys'] = table_l['w_tot']
 
-    kwargs = {'n_jobs': 40, 'comoving': False,
+    kwargs = {'n_jobs': 4, 'comoving': False,
               'cosmology': FlatLambdaCDM(H0=70, Om0=0.3)}
     if args.survey.lower() == 'cfht':
         kwargs['table_c'] = table_c
     if args.survey.lower() == 'kids':
         kwargs['nz_z'] = nz_z
-        kwargs['nz_z'] = nz_n
+        kwargs['nz_n'] = nz_n
         kwargs['use_nz'] = True
 
     print('Working on lenses in bin {}...'.format(lens_bin + 1))
@@ -165,10 +133,7 @@ for lens_bin in range(4):
         excess_surface_density, table_l_pre, **kwargs)
     result['ds_err'] = np.sqrt(np.diag(ds_cov))
 
-    fname_base = '{}_{}{}{}'.format(
-        args.survey.lower(), lens_bin,
-        '_no_dillution_correction' if args.no_dilution_correction else '',
-        ('_nstar_' + args.nstar) if args.nstar is not None else '')
+    fname_base = '{}_{}'.format(args.survey.lower(), lens_bin)
 
     np.savetxt(os.path.join('results', fname_base + '_cov.csv'), ds_cov)
 
