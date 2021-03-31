d181f42faffeaf856acbe88834f65101b243b3f4
Johannes Lange <jolange@ucsc.edu>
2021-03-31 16:15:35 -0700

    further udpated requirements

--- lux/init.sh
+++ lux/init.sh
@@ -1,9 +1,12 @@
 module load openmpi
 module load python/3.6.7
 module load blas
+module load intel/impi
+module load intel/ifort
 
 export PYTHONPATH="${PYTHONPATH}:/data/groups/leauthaud/jolange/Zebu/"
 
 source env/bin/activate
 
 export HDF5_USE_FILE_LOCKING='FALSE'
+
