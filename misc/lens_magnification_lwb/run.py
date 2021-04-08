922eebcb35c63db5d0276f6b3ba0c8ea5e731bbd
Johannes Lange <jolange@ucsc.edu>
2021-04-08 14:15:35 -0700

    updated lwb lens magnification calculation

--- misc/lens_magnification_lwb/run.py
+++ misc/lens_magnification_lwb/run.py
@@ -11,9 +11,14 @@ WHERE
   spec.z BETWEEN 0.54 AND 0.7
 """
 
+import camb
 import numpy as np
 import matplotlib.pyplot as plt
 from astropy.table import Table
+from astropy.cosmology import FlatLambdaCDM
+
+from dsigma.physics import critical_surface_density
+from dsigma.physics import lens_magnification_shear_bias
 
 # %%
 
@@ -43,3 +48,37 @@ plt.title('CMASS $0.54 - 0.7$')
 plt.tight_layout(pad=0.3)
 plt.savefig('cmass_flux_dist.pdf')
 plt.savefig('cmass_flux_dist.png', dpi=300)
+plt.close()
+
+# %%
+
+table = Table.read('hod_model_saito2016_mdpl2.txt', format='ascii')
+rp = table['col1']
+ds_full = table['col2'] * 0.8
+
+z_l = 0.6
+z_s = 0.8
+
+cosmo = FlatLambdaCDM(70, 0.3)
+theta = rp / cosmo.angular_diameter_distance(z_l).value
+
+pars = camb.CAMBparams()
+pars.set_cosmology(H0=67.66, ombh2=0.02242, omch2=0.11933)
+pars.InitPower.set_params(ns=0.9665, As=2.105e-9)
+pars.set_matter_power(redshifts=np.linspace(z_l, 0, 10), kmax=2000.0,
+                      nonlinear=True)
+
+camb_results = camb.get_results(pars)
+
+gamma = np.array([lens_magnification_shear_bias(
+    theta_i, 3.0, z_l, z_s, camb_results) for theta_i in theta])
+ds_lm = gamma * critical_surface_density(z_l, z_s, cosmology=cosmo,
+                                         comoving=False)
+
+plt.plot(rp, ds_lm / ds_full)
+plt.xscale('log')
+plt.xlabel(r'Projected Radius $r_p \, [\mathrm{Mpc}]$')
+plt.ylabel(r'$\Delta\Sigma_{\mathrm{lm}} / \Delta\Sigma{\mathrm{tot}}$')
+plt.tight_layout(pad=0.8)
+plt.savefig('bias.pdf')
+plt.savefig('bias.png', dpi=300)
