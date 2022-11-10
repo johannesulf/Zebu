0db8f6357c8619952be1da7c8bb6e87b0df7c67f
Johannes U. Lange <julange.astro@pm.me>
2022-11-10 10:07:23 -0500

    updated and simplified lux scripts

---
+++ lux/compute.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --partition=cpuq
+#SBATCH --account=cpuq
+#SBATCH --job-name=lensing_mock_challenge
+#SBATCH --nodes=1
+#SBATCH --ntasks=40
+#SBATCH --cpus-per-task=1
+#SBATCH --time=1-0:00:00
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=jolange@ucsc.edu
+#SBATCH --output=log/compute_%a.out
+
+cd /data/groups/leauthaud/jolange/Zebu/lux
+source init.sh
+cd ../stacks/
+python compute.py $SLURM_ARRAY_TASK_ID
