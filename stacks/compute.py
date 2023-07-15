1354934679579d3f36dd13f357e0c62a3b27b105
Johannes U. Lange <julange.astro@pm.me>
2023-07-14 21:19:41 -0400

    fixed crash

--- stacks/compute.py
+++ stacks/compute.py
@@ -20,7 +20,7 @@ multiprocessing.process.current_process()._config['tempdir'] = '/dev/shm'
 
 parser = argparse.ArgumentParser()
 parser.add_argument('config', type=int, help='configuration number',
-                    choices=range(72))
+                    choices=range(78))
 args = parser.parse_args()
 
 path = Path('results', '{}'.format(args.config))
