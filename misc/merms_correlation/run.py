4c8c17eeb1447319c1bdda04d505c450ad7fa57c
Johannes U. Lange <julange.astro@pm.me>
2024-02-15 10:16:47 -0500

    updated misc/merms_correlation

--- misc/merms_correlation/run.py
+++ misc/merms_correlation/run.py
@@ -3,11 +3,12 @@ import numpy as np
 
 # %%
 
-table_s = zebu.read_raw_data(1, 'source', 2, survey='hsc')
+table_s = zebu.read_mock_catalog('hsc', zebu.MOCK_PATH / 'buzzard-4',
+                                 zebu.PIXELS)
 
 # %%
 
-print('(1 + <m>)/(1 - <e_rms^2>): {:.5f}'.format(
-    (1 + np.mean(table_s['m'])) / (1 - np.mean(table_s['sigma_rms']**2))))
-print('<(1 + m)/(1 - e_rms^2)>: {:.5f}'.format(
-    np.mean((1 + table_s['m']) / (1 - table_s['sigma_rms']**2))))
+print('1 / (1 + <m>) (1 - <e_rms^2>): {:.5f}'.format(
+    1.0 / (1 + np.mean(table_s['m'])) / (1 - np.mean(table_s['e_rms']**2))))
+print('1 / <(1 + m) (1 - e_rms^2)>: {:.5f}'.format(
+    1.0 / np.mean((1 + table_s['m']) * (1 - table_s['e_rms']**2))))
